e16b28063437bc92489021824df255025abb65c4
AndresEdSu <102969654+AndresEdSu@users.noreply.github.com>
2024-09-30 17:58:51 -0400

    Update Proyecto Tula Tic_Tac_Toe.py

--- Proyecto Tula Tic_Tac_Toe.py
+++ Proyecto Tula Tic_Tac_Toe.py
@@ -1,23 +1,30 @@
 import pygame
 
+#initialize all imported pygame modules
 pygame.init()
 
 WINDOW_WIDTH = 720
 PIXEL_WIDTH = WINDOW_WIDTH // 3
 
+#Initialize a window or screen for display
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_WIDTH))
+
+#create an object to help track time
 clock = pygame.time.Clock()
 
 
 running = True
 
 
-
+#Definimos una función para cargar un ícono y ajustarlo a una determinada resolución
 def load_icon(path, resolution):
     icon = pygame.image.load(path)
+    #resize to new resolution
     return pygame.transform.scale(icon, resolution)
 
+#Ejecutamos la función load_icon
 GRID = load_icon('grid.png', [WINDOW_WIDTH, WINDOW_WIDTH])
+
 
 
 
